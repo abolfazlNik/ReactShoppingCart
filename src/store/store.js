4d0f912f4e4dffa3d9024bbc42340bb8a03f6c39
abolfazl251378 <abolfazlnikk@gmail.com>
2024-03-08 12:50:04 +0330

    Implement local storage middleware

--- src/store/store.js
+++ src/store/store.js
@@ -2,14 +2,21 @@ import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./slices/cartSlice";
 import { setupListeners } from "@reduxjs/toolkit/query";
 
+const localStorageMiddleware = (store) => (next) => (action) => {
+  const result = next(action);
+  const state = store.getState();
+  localStorage.setItem("cart", JSON.stringify(state.cart.cart));
+  return result;
+};
+
 export const store = configureStore({
   reducer: {
     cart: cartReducer,
   },
-});
-store.subscribe(() => {
-  const state = store.getState();
-  localStorage.setItem("cart", JSON.stringify(state.cart.cart));
+  middleware: (getDefaultMiddleware) => [
+    ...getDefaultMiddleware(),
+    localStorageMiddleware,
+  ],
 });
 
 setupListeners(store.dispatch);
